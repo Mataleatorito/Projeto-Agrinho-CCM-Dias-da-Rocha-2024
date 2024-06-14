ef1e3cc149fd46e8cf684c64966dbdd3e0b16646
o-luiz0 <abelha1904@hotmail.com>
2024-06-13 21:55:53 -0300

    Atualização do Site 13/06/2024
    Foi adicionado :
       - Imagens de template;
       - Identação em todo o código do site;
       - Personalização de Imagens;
       - Correção de Erros.

--- script.js
+++ script.js
@@ -1,17 +1,21 @@
-document.addEventListener('DOMContentLoaded', function() { // Aguarda o carregamento completo do DOM
+document.addEventListener('DOMContentLoaded', function () { // Aguarda o carregamento completo do DOM
     const menuButton = document.getElementById('menuButton'); // Seleciona o botão do menu
     const navMenu = document.getElementById('navMenu'); // Seleciona o menu de navegação
 
-    menuButton.addEventListener('click', function() { // Adiciona um evento de clique ao botão do menu
+    menuButton.addEventListener('click', function () { // Adiciona um evento de clique ao botão do menu
         navMenu.classList.toggle('shown'); // Alterna a classe "shown" no menu de navegação
         menuButton.setAttribute('aria-expanded', navMenu.classList.contains('shown')); // Atualiza o atributo aria-expanded com base na visibilidade do menu
     });
 
     // Fecha o menu ao clicar fora dele
-    document.addEventListener('click', function(event) { // Adiciona um evento de clique ao documento
+    document.addEventListener('click', function (event) { // Adiciona um evento de clique ao documento
         if (!navMenu.contains(event.target) && !menuButton.contains(event.target)) { // Verifica se o clique não foi no menu ou no botão
             navMenu.classList.remove('shown'); // Remove a classe "shown" do menu
             menuButton.setAttribute('aria-expanded', 'false'); // Atualiza o atributo aria-expanded para false
         }
     });
 });
+
+
+
+
