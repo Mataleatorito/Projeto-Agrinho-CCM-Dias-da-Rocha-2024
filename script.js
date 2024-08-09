8fbade61d809762075361b96c07451c7f80cf152
Widney Dias <widney7311@gmail.com>
2024-08-09 19:22:08 -0300

    responsividade do menu

--- script.js
+++ script.js
@@ -1,20 +1,28 @@
-document.addEventListener('DOMContentLoaded', function () { // Aguarda o carregamento completo do DOM
-    const menuButton = document.getElementById('menuButton'); // Seleciona o botão do menu
-    const navMenu = document.getElementById('navMenu'); // Seleciona o menu de navegação
+document.addEventListener('DOMContentLoaded', function () {
+    const menuButton = document.getElementById('menuButton');
+    const navMenu = document.getElementById('navMenu');
 
-    menuButton.addEventListener('click', function () { // Adiciona um evento de clique ao botão do menu
-        navMenu.classList.toggle('shown'); // Alterna a classe "shown" no menu de navegação
-        menuButton.setAttribute('aria-expanded', navMenu.classList.contains('shown')); // Atualiza o atributo aria-expanded com base na visibilidade do menu
+    menuButton.addEventListener('click', function () {
+        navMenu.classList.toggle('shown');
+        menuButton.setAttribute('aria-expanded', navMenu.classList.contains('shown'));
+
+        if (window.innerWidth <= 768) {
+            document.body.classList.toggle('no-scroll', navMenu.classList.contains('shown'));
+        }
     });
 
-    // Fecha o menu ao clicar fora dele
-    document.addEventListener('click', function (event) { // Adiciona um evento de clique ao documento
-        if (!navMenu.contains(event.target) && !menuButton.contains(event.target)) { // Verifica se o clique não foi no menu ou no botão
-            navMenu.classList.remove('shown'); // Remove a classe "shown" do menu
-            menuButton.setAttribute('aria-expanded', 'false'); // Atualiza o atributo aria-expanded para false
+    document.addEventListener('click', function (event) {
+        if (!navMenu.contains(event.target) && !menuButton.contains(event.target)) {
+            navMenu.classList.remove('shown');
+            menuButton.setAttribute('aria-expanded', 'false');
+
+            if (window.innerWidth <= 768) {
+                document.body.classList.remove('no-scroll');
+            }
         }
     });
 });
+
 
 document.addEventListener('DOMContentLoaded', function() {
     const addToCartButtons = document.querySelectorAll('.add-to-cart');
@@ -63,10 +71,3 @@ document.addEventListener('DOMContentLoaded', function() {
         totalValueText.textContent = `Valor Total: R$ ${total.toFixed(2)}`;
     }
 });
-
-
-
-
-
-
-
